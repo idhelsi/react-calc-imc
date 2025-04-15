92cc3be1374ffc1d13c19fe13db5996129bbb003
Helsi <helsi_@hotmail.com>
2025-04-15 13:06:29 -0400

    update img

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import  styles  from "./App.module.css";
-import poweredImage from "./assets/powered.png";
+
 import leftArrowIMage from "./assets/leftarrow.png";
 import { GridItem } from './components/Griditem'
 
@@ -29,7 +29,7 @@ export const App = () => {
     <div className={styles.main}>
       <header>
         <div className={styles.headerContainer}>
-          <img src={poweredImage} alt='' width={150}/>
+          
         </div>
       </header>
       <div className={styles.container}>
